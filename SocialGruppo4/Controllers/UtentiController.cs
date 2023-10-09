6f1d5aabe4d021d8e57b66555a46d6ab61d9713f
Vasile Blanaru <vasile.blanaru@outlook.it>
2023-10-09 10:32:14 +0200

    Aggiunge funzionalità follow
    Il tasto Segui in Home ora funziona. Chiama l'azione Follow ed inserisce
    il record in database.

--- SocialGruppo4/Controllers/UtentiController.cs
+++ SocialGruppo4/Controllers/UtentiController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SocialGruppo4.Models.Followers;
 using SocialGruppo4.Models.Utenti;
 using Utility;
 
@@ -6,7 +7,7 @@ namespace SocialGruppo4.Controllers
 {
     public class UtentiController : Controller
     {
-        public ActionResult ElencoUtenti()
+        public IActionResult ElencoUtenti()
         {
             List<Entity> e = DAOUtenti.GetInstance().Read();
 
@@ -16,7 +17,7 @@ namespace SocialGruppo4.Controllers
                 Utente utente = new Utente
                 {
                     Nominativo = ((Utente)entitaItem).Nominativo,
-                    Email = ((Utente)entitaItem).Email 
+                    Email = ((Utente)entitaItem).Email
                 };
                 utenti.Add(utente);
             }
@@ -24,6 +25,50 @@ namespace SocialGruppo4.Controllers
             return View(utenti);
         }
 
+        [HttpPost("utenti/follow/{id:int}")]
+        public IActionResult Follow(int id)
+        {
+            Entity? utente = (Entity?)HttpContext.Items["User"];
 
+            if (utente is null)
+            {
+                return RedirectToAction("Index", "Auth");
+            }
+
+            Utente? uFollow = (Utente?)DAOUtenti.GetInstance().Find(id);
+            if (uFollow is null)
+            {
+                TempData["FlashMessage"] = new Dictionary<string, string>{
+                    {"Status", "error"},
+                    {"Message", "Utente da seguire non trovato."}
+                };
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            Follower follower = new()
+            {
+                IdUtente = utente.Id,
+                IdFollower = id,
+            };
+
+            bool success = DAOFollower.getInstance().Insert(follower);
+            if (!success)
+            {
+                TempData["FlashMessage"] = new Dictionary<string, string>{
+                    {"Status", "error"},
+                    {"Message", "Stai già seguendo questo utente."}
+                };
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            TempData["FlashMessage"] = new Dictionary<string, string>{
+                {"Status", "success"},
+                {"Message", $"Fatto! Da ora segui {uFollow?.Nominativo}."}
+            };
+
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
