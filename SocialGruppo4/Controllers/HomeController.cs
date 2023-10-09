cf9c743ec7ae89b369044a7d7340750f3956a4ad
Vasile Blanaru <vasile.blanaru@outlook.it>
2023-10-09 17:50:49 +0200

    Salva le immagini del nuovo post

--- SocialGruppo4/Controllers/HomeController.cs
+++ SocialGruppo4/Controllers/HomeController.cs
@@ -9,10 +9,12 @@ namespace SocialGruppo4.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IHostEnvironment _env;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IHostEnvironment env)
     {
         _logger = logger;
+        _env = env;
     }
 
     public IActionResult Index()
@@ -28,6 +30,21 @@ public class HomeController : Controller
     public IActionResult Privacy()
     {
         return View();
+    }
+
+    public IActionResult CreaPost()
+    {
+        IFormFile? imageFile = Request.Form.Files["imageFile"];
+        if (imageFile is not null)
+        {
+            string path = Path.Combine(_env.ContentRootPath, "wwwroot", "images", "posts", imageFile.FileName);
+            using var fileStream = new FileStream(path, FileMode.Create);
+            imageFile.CopyTo(fileStream);
+        }
+
+        Console.WriteLine(Request.Form["content"]);
+
+        return RedirectToAction("Index");
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
