f5ff0b148514ce2cb72fee55193f57d8c32f0e57
Vasile Blanaru <vasile.blanaru@outlook.it>
2023-10-11 14:46:27 +0200

    Aggiunge i cognomi agli utenti di default

--- Database.sql
+++ Database.sql
@@ -21,13 +21,13 @@ create table Utenti
 	passwordHash varchar(64)
 );
 
-insert into Utenti (nominativo, amministratore, email, numero, residenza, codiceFiscale, passwordHash)
-values
-	('Lorenzo',1, 'lorenzo@example.com', '1234567890', '123 Main Street, City1', 'ABCD123EFGH56789I', HASHBYTES('Sha2_512','password123')),
-	('Vasile', 0,'vasile@example.com', '9876543210', '456 Elm Street, City2', 'WXYZ456JKLM78901N', HASHBYTES('Sha2_512','password123')),
-	('Emanuele', 0,'emanuele@example.com', '5555555555', '789 Oak Street, City3', 'PQRS6789TUVW12345X', HASHBYTES('Sha2_512','password123')),
-	('Shalva', 0, 'shalva@example.com', '1112223333', '101 Pine Street, City4', 'EFGH6789IJKL12345M', HASHBYTES('Sha2_512','password123')),
-	('Francesco', 0, 'francesco@example.com', '9998887777', '321 Cedar Street, City5', 'MNOP1234QRST56789U', HASHBYTES('Sha2_512','password123'));
+INSERT INTO Utenti (nominativo, amministratore, email, numero, residenza, codiceFiscale, passwordHash)
+VALUES
+	('Lorenzo Cuvertino',1, 'lorenzo@example.com', '1234567890', '123 Main Street, City1', 'ABCD123EFGH56789I', HASHBYTES('Sha2_512','password123')),
+	('Vasile Blanaru', 1, 'vasile@example.com', '9876543210', '456 Elm Street, City2', 'WXYZ456JKLM78901N', HASHBYTES('Sha2_512','password123')),
+	('Emanuele Zonetti', 0, 'emanuele@example.com', '5555555555', '789 Oak Street, City3', 'PQRS6789TUVW12345X', HASHBYTES('Sha2_512','password123')),
+	('Shalva Arabuli', 0, 'shalva@example.com', '1112223333', '101 Pine Street, City4', 'EFGH6789IJKL12345M', HASHBYTES('Sha2_512','password123')),
+	('Francesco Vicario', 0, 'francesco@example.com', '9998887777', '321 Cedar Street, City5', 'MNOP1234QRST56789U', HASHBYTES('Sha2_512','password123'));
 
 select * from Utenti
 
@@ -47,7 +47,8 @@ create table Posts
 insert into Posts (idUtente, idPadre, contenuto, dataEora, miPiace, immagine)
 values
 	(1, NULL, 'Oggi è proprio una bella giornata', '03-10-2023 12:07', 0, NULL),
-	(2, 1, 'Questo è un commento al primo post', '11-10-2023 14:19', 3, '');
+	(2, 1, 'Questo è un commento al primo post', '11-10-2023 14:19', 3, ''),
+	(2, NULL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', '11-10-2023 14:19', 14, '');
 
 select * from Posts
 
