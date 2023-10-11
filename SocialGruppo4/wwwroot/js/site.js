986888c96ea7b12d6b5562f540b0454f598ff962
Vasile Blanaru <vasile.blanaru@outlook.it>
2023-10-11 12:04:20 +0200

    Cambia lo stile dei tweet in home

--- SocialGruppo4/wwwroot/js/site.js
+++ SocialGruppo4/wwwroot/js/site.js
@@ -1,3 +1,13 @@
 ﻿function closeFlashMessage(event) {
     event.target.closest(".flash-message").style.display = "none";
 }
+
+function fillLike(event) {
+    let likeSvg = event.target.querySelector("svg");
+    likeSvg.setAttribute("fill", "currentColor");
+}
+
+function unFillLike(event) {
+    let likeSvg = event.target.querySelector("svg");
+    likeSvg.setAttribute("fill", "none");
+}
